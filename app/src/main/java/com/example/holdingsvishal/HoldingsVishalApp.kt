7ce1b419da1f478259b1c5c1c396b74dc228ac18
vishal.neosoft <vishal.parmar@neosoftmail.com>
2025-06-10 17:06:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/holdingsvishal/HoldingsVishalApp.kt
@@ -0,0 +1,22 @@
+package com.example.holdingsvishal
+
+import android.app.Application
+
+class HoldingsVishalApp : Application() {
+
+    companion object {
+
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        init()
+    }
+
+
+    private fun init() {
+
+    }
+
+}
